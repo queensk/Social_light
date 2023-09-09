324bfffe832e83ce69a768512ee0ea646678c1ad
queens kisivuli <queenskisivuli@gmail.com>
2023-09-09 12:19:11 +0300

    main app

---
+++ Social_Light_Auth/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Social_Light_Auth.Data;
+using Social_Light_Auth.Extensions;
+using Social_Light_Auth.Models;
+using Social_Light_Auth.Models.DTO.JWToptions;
+using Social_Light_Auth.Service;
+using Social_Light_Auth.Service.IService;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+
+// dependacy injections
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IJWtTokenGenerator, JwtService>();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("JwtOptions"));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseMigration();
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
