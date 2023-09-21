b1bb7c20d411705bc6dbaa506849b5877e9009a3
Wesley254 <wesleykirui2021@gmail.com>
2023-09-21 13:46:06 +0300

    fixed errors on Login page

--- Social_light_Frontend/Models/Auth/LoginRequestDto.cs
+++ Social_light_Frontend/Models/Auth/LoginRequestDto.cs
@@ -8,7 +8,8 @@ namespace Social_light_Frontend.Models.Auth
 {
     public class LoginRequestDto
     {
-        public required string Email { get; set; }
-        public required string Password { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
--- Social_light_Frontend/Models/Auth/RegisterRequestDto.cs
+++ Social_light_Frontend/Models/Auth/RegisterRequestDto.cs
@@ -8,8 +8,8 @@ namespace Social_light_Frontend.Models.Auth
 {
     public class RegisterRequestDto
     {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
